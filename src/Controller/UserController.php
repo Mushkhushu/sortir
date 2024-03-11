81065553033f07d36b3948aa0db936f37bd837f0
Mushkhushu <flavie.houlette@laposte.net>
2024-03-11 20:28:10 +0100

    add upload avatar et modif des fronts pour la bare de user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,23 +8,27 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
-#[Route('/user')]
+#[Route(path: 'user/', name: 'user_')]
 class UserController extends AbstractController
 {
+    public function getUsersInfos(Security $security): Response
+    {
+        $user = $security->getUser();
+        $userName = $user?->getUsername();
 
+        return $this->render('base.html.twig', [
+            'username' => $userName, 'user' => $user
+        ]);
+    }
 
-
-
-
-
-
-
-
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    #[Route('', name: 'index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -32,7 +36,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -52,33 +56,65 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
+    #[Route('show', name: 'show', methods: ['GET'])]
+    public function show(Security $security): Response
     {
+        $user = $security->getUser();
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    #[Route('edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Security $security,Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
+        $user = $security->getUser();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            if (!empty($formData->getUsername())) {
+               $user->setUsername($formData->getUsername());
+            }
+            if (!empty($formData->getFirstName())) {
+                $user->setFirstName($formData->getFirstName());
+            }
+            if (!empty($formData->getlastName())) {
+                $user->setLastName($formData->getLastName());
+            }
+            if (!empty($formData->getPhoneNumber())) {
+                $user->setPhoneNumber($formData->getPhoneNumber());
+            }
+            if (!empty($formData->getMail())) {
+                $user->setMail($formData->getMail());
+            }
+            if ($form->get('picture')->getData() instanceof UploadedFile) {
+                $pictureFile = $form->get('picture')->getData();
+                $fileName = $slugger->slug($user->getUsername()) . '-' . uniqid() . '.' . $pictureFile->guessExtension();
+                $pictureFile->move($this->getParameter('picture_dir'), $fileName);
+                if (!empty($user->getPicture())) {
+                    $picturePath= $this->getParameter('picture_dir') . '/' . $user->getPicture();
+                    if (file_exists($picturePath)) {
+                        unlink($picturePath);
+                    }
+                }
+                $user->setPicture($fileName);
+            }
+            $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('success', 'Le profil a été mis à jour avec succès !');
+            return $this->redirectToRoute('user_show');
         }
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('delete', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
